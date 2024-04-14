32f11cab5e0c3b0b16c2a7de4f80d8342f8aee57
MCCLUCKY415 <ruthenavelu415@gmail.com>
2024-04-14 22:08:27 +0200

    Updated the Steps class to have the array for the user to put in a description for each step.

--- ST10256859_PROG6221_POE_PART1/Classes/Steps.cs
+++ ST10256859_PROG6221_POE_PART1/Classes/Steps.cs
@@ -10,5 +10,18 @@ namespace ST10256859_PROG6221_POE_PART1.Classes
     {
         public string StepDescription { get; set; }
         public int NumOfSteps { get; set; }
+
+        public Steps[] GetSteps(int numSteps)
+        {
+            Steps[] stp = new Steps[numSteps];
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.Write("Please enter a description for step " + (i + 1) + ": ");
+                string description = Console.ReadLine();
+
+                stp[i] = new Steps { StepDescription = description };
+            }
+            return stp;
+        }
     }
 }
