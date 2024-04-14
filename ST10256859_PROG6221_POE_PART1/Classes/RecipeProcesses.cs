13cd09f962c63ffa250f205eef581abba9f8312e
MCCLUCKY415 <ruthenavelu415@gmail.com>
2024-04-14 23:25:46 +0200

    Updated the RecipeProcesses class. it now has a main menu method that will interact with the user.

--- ST10256859_PROG6221_POE_PART1/Classes/RecipeProcesses.cs
+++ ST10256859_PROG6221_POE_PART1/Classes/RecipeProcesses.cs
@@ -4,8 +4,8 @@ namespace ST10256859_PROG6221_POE_PART1.Classes
 {
     public class RecipeProcesses
     {
-        Ingredients ing = new Ingredients();
-        Steps stp = new Steps();
+        private Ingredients ing = new Ingredients();
+        private Steps stp = new Steps();
 
         public void ScaleRecipe(Ingredients[] ing, double factor)
         {
@@ -23,10 +23,16 @@ namespace ST10256859_PROG6221_POE_PART1.Classes
             }
         }
 
+        public void ClearAllData()
+        {
+            ing = new Ingredients();
+            stp = new Steps();
+        }
+
         public void DisplayRecipe(Ingredients[] ing, Steps[] steps)
         {
             Console.WriteLine("\n            RECIPE            ");
-            Console.WriteLine("******************************");
+            Console.WriteLine("*******************************");
             Console.WriteLine("Ingredients:");
             foreach (var ingredient in ing)
             {
@@ -37,6 +43,48 @@ namespace ST10256859_PROG6221_POE_PART1.Classes
             foreach (var step in steps)
             {
                 Console.WriteLine(step.StepDescription);
+            }
+        }
+
+        public void MainMenu(Ingredients[] ing, Steps[] stp)
+        {
+            while (true)
+            {
+                DisplayRecipe(ing, stp);
+
+                Console.WriteLine("\n            MENU            ");
+                Console.WriteLine("*****************************");
+                Console.WriteLine("1) Enter a new recipe");
+                Console.WriteLine("2) Scale recipe");
+                Console.WriteLine("3) Reset quantities");
+                Console.WriteLine("4) Clear all data");
+                Console.WriteLine("5) Exit");
+                Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    // Code to handle entering a new recipe
+                    break;
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Please enter the number you would like to scale the recipe by (Put in 0.5 for half, 2 for double or 3 for triple): ");
+                    double factor = Convert.ToDouble(Console.ReadLine());
+                    ScaleRecipe(ing, factor);
+                }
+                else if (choice == 3)
+                {
+                    ResetNewQuantities(ing);
+                }
+                else if (choice == 4)
+                {
+                    ClearAllData();
+                }
+                else if (choice == 5)
+                {
+                    return;
+                }
             }
         }
     }
